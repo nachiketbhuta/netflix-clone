d27091d2d947d61d3616517cae2065bf68ffe60f
Nachiket Bhuta <nachiketbhuta@gmail.com>
2022-12-17 01:19:51 +0530

    Initial Commit

---
+++ Netlfix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netlfix Clone
+//
+//  Created by Nachiket Bhuta on 17/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
